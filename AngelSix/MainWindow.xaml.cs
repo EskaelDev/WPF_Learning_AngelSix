f2b83d4c85e101b2cf2b27e4859bf78b8a333c48
Natsyu <rainbowvme@gmail.com>
2019-05-01 17:54:58 +0200

    01 done

--- AngelSix/MainWindow.xaml.cs
+++ AngelSix/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace AngelSix
         {
             InitializeComponent();
         }
+
+        
+
+        private void ApplyButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The description is {this.DescriptionText.Text}");
+        }
+
+        private void ResetButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.WeldCheckBox.IsChecked = this.AssemblyCheckBox.IsChecked = this.PlasmaCheckBox.IsChecked = this.LaserCheckBox.IsChecked = this.PurchaseCheckBox.IsChecked = this.LatheCheckBox.IsChecked = this.DrillCheckBox.IsChecked = this.FoldCheckBox.IsChecked = this.RollCheckBox.IsChecked = this.SawCheckBox.IsChecked = false;
+        }
+
+        private void CheckboxChecked(object sender, RoutedEventArgs e)
+        {
+            this.LengthText.Text += " ";
+            this.LengthText.Text += ((CheckBox) sender).Content;
+        }
+
+        private void FinishComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.NoteText == null)
+                return;
+            this.NoteText.Text = (string)((ComboBoxItem)((ComboBox) sender).SelectedValue).Content;
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            FinishComboBox_OnSelectionChanged(FinishComboBox, null);
+        }
     }
 }
